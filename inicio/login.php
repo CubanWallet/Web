242969d63aacdaa4e3d3bd68c5f17f8d41b2d89c
CubanWallet <L.eogarciaguerrero04@gmail.com>
2024-08-01 00:26:17 -0400

    Add files via upload

---
+++ inicio/login.php
@@ -0,0 +1,28 @@
+
+</head>
+<body>
+  <style>
+
+
+  </style>
+<div class="contenedor">
+    <p class="wallet-logo">Mi Wallet</p>
+
+    <form id="form-login">
+        <div class="input-group">
+            <label for="usuario">Nombre de usuario</label>
+            <input type="text" id="usuario" placeholder="Nombre de usuario" required>
+        </div>
+        <div class="input-group">
+            <label for="contraseña">Contraseña</label>
+            <input type="password" id="contraseña" placeholder="Contraseña" required>
+        </div>
+        <button type="submit">Iniciar sesión</button>
+    </form>
+
+    <div class="registro">
+        <p>¿No tienes una cuenta? <a href="registro.html">Regístrate aquí</a></p>
+    </div>
+</div>
+
+<script src="js
